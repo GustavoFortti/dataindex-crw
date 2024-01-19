74d74fb3bf0b04c8d86ec2c40c938b9f0276eb76
Gustavo Fortti <gustavofortti@gmail.com>
2024-01-19 18:52:53 -0300

    fix dark_lab seed

--- launcher.sh
+++ launcher.sh
@@ -21,16 +21,16 @@
 
 source ./.env/setup.sh
 
-# python3 main.py darkness extract --option init
-# python3 main.py dux_nutrition_lab extract --option test_tag
+python3 main.py max_titanium extract --option init
+# python3 main.py max_titanium extract --option test_tag
 # python3 main.py darkness extract --option update_pages
 # python3 main.py darkness extract --option update_products
 # python3 main.py darkness dry --option init
 # python3 main.py darkness ingestion
 # python3 main.py _set_ ingestion
 
-marcas=("adaptogen" "atlhetica_nutrition" "black_skull" "boldsnacks" "dark_lab" "darkness" "dux_nutrition_lab" "growth_supplements" "integralmedica" "iridium_labs" "max_titanium" "new_millen" "nutrata" "probiotica" "under_labz")
-for marca in "${marcas[@]}"
-do
-    python3 main.py "$marca" --option init
-done
+# marcas=("adaptogen" "atlhetica_nutrition" "black_skull" "boldsnacks" "dark_lab" "darkness" "dux_nutrition_lab" "growth_supplements" "integralmedica" "iridium_labs" "max_titanium" "new_millen" "nutrata" "probiotica" "under_labz")
+# for marca in "${marcas[@]}"
+# do
+#     python3 main.py "$marca" extract --option test_tag
+# done
