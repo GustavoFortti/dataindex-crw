d35eb12ace0a6b8ab3cb72b23619c278680aa548
gustavofortti <gustavofortti@gmail.com>
2024-01-19 23:32:22 -0300

    fix dux dir e tempo de extracao de pags

--- shared/selenium_service.py
+++ shared/selenium_service.py
@@ -51,11 +51,11 @@ def get_html(driver, url, sleep=1, scroll_page=False, return_text=False):
             total_altura = driver.execute_script("return document.body.scrollHeight")
 
             while (total_altura > prox_altura):
-                prox_altura += 1000
-                atual_altura = prox_altura - 1000
+                prox_altura += 500
+                atual_altura = prox_altura - 500
                 driver.execute_script(f"window.scrollTo({atual_altura}, {prox_altura});")
                 print(f"{prox_altura}/{total_altura}")
-                time.sleep(0.7)
+                time.sleep(0.4)
         
         page_html = driver.page_source
         soup = BeautifulSoup(page_html, 'html.parser')
