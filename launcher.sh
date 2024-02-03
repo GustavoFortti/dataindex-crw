71d8b12fbc8d0a33347d1d3acdf1c70f747ac8b1
Gustavo Fortti <gustavofortti@gmail.com>
2024-02-03 00:59:21 -0300

    criação do launcher

--- launcher.sh
+++ launcher.sh
@@ -4,32 +4,50 @@ export LOCAL=/home/mage/main/dataindex-crw
 export DISPLAY=:1
 bash $LOCAL/setup.sh
 
-# adaptogen
-# atlhetica_nutrition
-# black_skull
-# boldsnacks
-# dark_lab
-# darkness
-# dux_nutrition_lab 
-# growth_supplements
-# integralmedica 
-# iridium_labs
-# max_titanium 
-# new_millen
-# nutrata
-# probiotica 
-# under_labz
+job_name=""
+job_type=""
+option=""
+page_type=""
+country=""
 
-python3 $LOCAL/main.py darkness extract --option init
-# python3 main.py max_titanium extract --option check_tag
-# python3 main.py darkness extract --option update_pages
-# python3 main.py darkness extract --option update_products
-# python3 main.py darkness dry --option default
-# python3 main.py darkness ingestion
-# python3 main.py _set_ ingestion
+usage() {
+  echo "Usage: $0 --job_name NAME --job_type TYPE --option OPTION --page_type PAGE_TYPE --country COUNTRY"
+  exit 1
+}
 
-# marcas=("adaptogen" "atlhetica_nutrition" "black_skull" "boldsnacks" "dark_lab" "darkness" "dux_nutrition_lab" "growth_supplements" "integralmedica" "iridium_labs" "max_titanium" "new_millen" "nutrata" "probiotica" "under_labz")
-# for marca in "${marcas[@]}"
-# do
-#     python3 main.py "$marca" extract --option init
-# done
+if [ "$#" -eq 0 ]; then
+    usage
+fi
+
+while [ "$1" != "" ]; do
+    case $1 in
+        --job_name )    shift
+                        job_name=$1
+                        ;;
+        --job_type )    shift
+                        job_type=$1
+                        ;;
+        --option )      shift
+                        option=$1
+                        ;;
+        --page_type )   shift
+                        page_type=$1
+                        ;;
+        --country )     shift
+                        country=$1
+                        ;;
+        * )             usage
+                        ;;
+    esac
+    shift
+done
+
+echo "Running with the following parameters:"
+echo "job_name: $job_name"
+echo "job_type: $job_type"
+echo "option: $option"
+echo "page_type: $page_type"
+echo "country: $country"
+
+echo "Command: ./launcher.sh --job_name $job_name --job_type $job_type --option $option --page_type $page_type --country $country"
+python3 --job_name $job_name --job_type $job_type --option $option --page_type $page_type --country $country --local $LOCAL
