52e37ddab655f276fe29cc1960b3734f092bc9f4
Gustavo Fortti <gustavofortti@gmail.com>
2024-03-24 15:11:07 -0300

    fix tag var nutrata

--- pages/supplement/brazil/nutrata/vars.py
+++ pages/supplement/brazil/nutrata/vars.py
@@ -5,6 +5,7 @@ PRODUCT_DEFINITION_TAG = [
     {'tag': 'span', 'class': 'tagged_as'}, 
     {'tag': 'nav', 'class': 'woocommerce-breadcrumb'}, 
     {'tag': 'div', 'class': 'woocommerce-Tabs-panel woocommerce-Tabs-panel--description panel entry-content wc-tab'}
+    {'tag': 'div', 'class': 'woocommerce-tabs et-clearfix et_element wc-tabs-wrapper type-accordion opened-all-tabs toggles-by-arrow loaded'}
 ]
 
 DYNAMIC_SCROLL = {
