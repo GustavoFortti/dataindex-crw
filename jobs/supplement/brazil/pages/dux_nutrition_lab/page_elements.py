96a6bc7673fa3ceed12e3ec8db1ebe65b3206156
Gustavo Fortti <gustavofortti@gmail.com>
2024-08-14 19:49:32 -0300

    C-40A372 | corrigir dux_nutrition_lab

--- jobs/supplement/brazil/pages/dux_nutrition_lab/page_elements.py
+++ jobs/supplement/brazil/pages/dux_nutrition_lab/page_elements.py
@@ -1,32 +1,31 @@
 def get_items(conf, soup):
-    items = soup.find_all('div', class_='vtex-search-result-3-x-galleryItem')
+    # Busca todos os itens no HTML com a classe especificada
+    items = soup.find_all('div', class_='duxnutrition-search-result-3-x-galleryItem')
     return items
 
 def get_product_url(conf, soup):
+    # Busca o link do produto no HTML
     product_link_element = soup.find('a', class_='vtex-product-summary-2-x-clearLink')
     product_link = conf["url"] + product_link_element['href'] if product_link_element else None
     return product_link
 
-
 def get_title(conf, soup):
+    # Busca o título do produto no HTML
     title_element = soup.find('span', class_='vtex-product-summary-2-x-productBrand')
     title = title_element.get_text().strip() if title_element else None
     return title
 
 def get_price(conf, soup):
+    # Busca o preço do produto no HTML
     price = None
-    price_element = soup.find('span', class_='vtex-product-price-1-x-sellingPriceValue vtex-product-price-1-x-sellingPriceValue--summary')
+    price_element = soup.find('span', class_='vtex-store-components-3-x-sellingPriceValue')
     if price_element:
-        currency_container = price_element.find('span', class_='vtex-product-price-1-x-currencyContainer')
-        if currency_container:
-            price = ''.join([elem.text for elem in currency_container.find_all('span')])
-            price = price.replace('\xa0', ' ').strip()
+        price = price_element.get_text().strip()
     return price
 
 def get_image_url(conf, soup):
-    image_div = soup.find('div', class_='duxnutrition-product-0-x-imageStackContainer')
-    if image_div:
-        image_element = image_div.find('img', class_='duxnutrition-product-0-x-imageNormal')
-        if image_element and 'src' in image_element.attrs:
-            return image_element['src']
+    # Busca a URL da imagem do produto no HTML
+    image_element = soup.find('img', class_='vtex-product-summary-2-x-imageNormal')
+    if image_element and 'src' in image_element.attrs:
+        return image_element['src']
     return None
