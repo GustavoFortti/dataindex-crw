7fcc42cf77118acefff63e6ce2f70609de85aae6
Gustavo Fortti <gustavofortti@gmail.com>
2024-09-30 20:28:52 -0300

    C-ACB062 | Organização do sistema

--- launcher.sh
+++ launcher.sh
@@ -5,40 +5,44 @@ page_type=""
 country=""
 mode="prd"
 
-job_type=false
+exec_type=false
 page_name=false
-option=false
+exec_flag=false
 
-local="/home/crw-system/dataindex-crw"
+local="/home/crw-system/shape-data-shelf-crw"
 
 usage() {
-    echo "Usage: $0 [OPTIONS]"
-    echo "Options:"
+    echo "Usage: $0 [exec_flagS]"
+    echo "exec_flags:"
+    echo "  --job_type NAME      Set the job type to TYPE"
     echo "  --job_name NAME      Set the job name to NAME"
-    echo "  --job_type TYPE      Set the job type to TYPE"
-    echo "  --option OPTION      Set the option to OPTION"
+    echo "  --exec_type TYPE      Set the exec type to exec"
+    echo "  --exec_flag exec_flag      Set the exec_flag to exec_flag"
     echo "  --page_name NAME     Set the page name to NAME"
     echo "  --page_type TYPE     Set the page type to TYPE"
     echo "  --country COUNTRY    Set the country to COUNTRY"
     echo "  --mode MODE          Set the mode to MODE"
     echo "  --local LOCAL        Set the local to LOCAL"
     echo
-    echo "Each option must be followed by its respective value."
+    echo "Each exec_flag must be followed by its respective value."
 }
 
 while [ "$1" != "" ]; do
     case $1 in
+        --job_type )    shift
+                        job_type=$1
+                        ;;
         --job_name )    shift
                         job_name=$1
                         ;;
         --page_name )    shift
                         page_name=$1
                         ;;
-        --job_type )    shift
-                        job_type=$1
+        --exec_type )    shift
+                        exec_type=$1
                         ;;
-        --option )      shift
-                        option=$1
+        --exec_flag )      shift
+                        exec_flag=$1
                         ;;
         --page_type )   shift
                         page_type=$1
@@ -82,20 +86,22 @@ else
 fi
 
 echo "Running with the following parameters:" >> "$log_file"
+echo "job_type: $job_type" >> "$log_file"
 echo "job_name: $job_name" >> "$log_file"
 echo "page_name: $page_name" >> "$log_file"
-echo "job_type: $job_type" >> "$log_file"
-echo "option: $option" >> "$log_file"
+echo "exec_type: $exec_type" >> "$log_file"
+echo "exec_flag: $exec_flag" >> "$log_file"
 echo "page_type: $page_type" >> "$log_file"
 echo "country: $country" >> "$log_file"
 echo "mode: $mode" >> "$log_file"
 echo "LOCAL: $LOCAL" >> "$log_file"
 echo "Job start: $(date '+%Y-%m-%d %H:%M:%S')" >> "$log_file"
 
-python3 "$LOCAL/main.py" --job_name "$job_name" \
+python3 "$LOCAL/main.py" --job_type "$job_type" \
+                         --job_name "$job_name" \
                          --page_name "$page_name" \
-                         --job_type "$job_type" \
-                         --option "$option" \
+                         --exec_type "$exec_type" \
+                         --exec_flag "$exec_flag" \
                          --page_type "$page_type" \
                          --country "$country" \
                          --mode "$mode" >> "$log_file" 2>&1
