4b664058135231b77a6e5312a80af1a43f7f87fc
Gustavo Fortti <gustavofortti@gmail.com>
2024-10-01 21:19:59 -0300

    F-34AAAE | adicionar configurar display ao launcher

--- src/lib/load/load.py
+++ src/lib/load/load.py
@@ -1,31 +1,40 @@
+from copy import deepcopy
+
 import pandas as pd
+
 import src.lib.utils.data_quality as dq
-from src.lib.utils.log import message
 from src.lib.load.connection.shopify import process_and_ingest_products
+from src.lib.utils.dataframe import create_or_read_df
+from src.lib.utils.log import message
+
 
 def load(conf):
-    print(conf['data_path'])
-    
     # Carregar os DataFrames e adicionar a coluna 'transform'
     df_products_transform_csl = pd.read_csv(conf['data_path'] + '/products_transform_csl.csv')
     df_products_transform_csl['is_transform_data'] = 1
     
-    df_products_load_csl = pd.read_csv(conf['data_path'] + '/products_load_csl.csv')
-    df_products_load_csl['is_transform_data'] = 0
+    products_load_csl_path = conf['data_path'] + '/products_load_csl.csv'
+    
+    df_products_load_csl = create_or_read_df(products_load_csl_path, df_products_transform_csl.columns)
+    if (not df_products_load_csl.empty):
+        df_products_load_csl['is_transform_data'] = 0
 
-    # Unir os DataFrames
-    df_union = pd.concat([df_products_transform_csl, df_products_load_csl])
+        # Unir os DataFrames
+        df_union = pd.concat([df_products_transform_csl, df_products_load_csl])
 
-    # Remover a coluna 'is_transform_data' apenas para identificar duplicatas
-    df_union_no_transform = df_union.drop(columns=['is_transform_data'])
+        # Remover a coluna 'is_transform_data' apenas para identificar duplicatas
+        df_union_no_transform = df_union.drop(columns=['is_transform_data'])
 
-    # Identificar as duplicatas sem considerar a coluna 'is_transform_data'
-    duplicates = df_union_no_transform[df_union_no_transform.duplicated(keep=False)]
+        # Identificar as duplicatas sem considerar a coluna 'is_transform_data'
+        duplicates = df_union_no_transform[df_union_no_transform.duplicated(keep=False)]
 
-    # Remover duplicatas e suas originais do DataFrame original (com 'is_transform_data')
-    df = df_union.drop(duplicates.index)
-    df = df[df['is_transform_data'] == 1]
-    df = df.drop(columns=['is_transform_data'])
+        # Remover duplicatas e suas originais do DataFrame original (com 'is_transform_data')
+        df = df_union.drop(duplicates.index)
+        df = df.drop(columns=['is_transform_data'])
+        df = df[df['is_transform_data'] == 1]
+    else:
+        df = deepcopy(df_products_transform_csl)
+        df = df.drop(columns=['is_transform_data'])
 
     df_products_transform_csl = df_products_transform_csl.drop(columns=['is_transform_data'])
     
