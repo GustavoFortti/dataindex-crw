662a90b51f99ba0ddafa089ad6432b06f1d8f863
Gustavo Fortti <gustavofortti@gmail.com>
2024-10-03 12:35:07 -0300

    F-827A3C | adicionar oficial farma

--- src/jobs/slave_page/pages/brazil/oficialfarma/conf.py
+++ src/jobs/slave_page/pages/brazil/oficialfarma/conf.py
@@ -4,14 +4,14 @@ URL = "https://www.oficialfarma.com.br/"
 STATUS = False
 
 PRODUCT_DEFINITION_TAG_MAP = [
-    {'tag': None, 'path': ''},
+    {'tag': None, 'path': '#maincontent > div.columns > div > div.product.attribute.description > div > div > div'},
 ]
 
 DYNAMIC_SCROLL = {
-    "time_sleep": 0.7,
-    "scroll_step": 1500,
+    "time_sleep": 1.3,
+    "scroll_step": 700,
     "percentage": 0.07,
-    "return_percentage": 0.1,
+    "return_percentage": 0.3,
     "max_return": 2000,
     "max_attempts": 3
 }
