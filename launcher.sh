b5f69add4c500b75d60c8d4c106ae202db596702
Gustavo Fortti <gustavofortti@gmail.com>
2024-10-08 06:15:07 -0300

    erro no display

--- launcher.sh
+++ launcher.sh
@@ -12,7 +12,7 @@ local="/home/crw-system/shape-data-shelf-crw"
 export USE_HEADLESS="false"
 export CHECKPOINT_EXTRACT="true"
 
-export DISPLAY="0:0"
+export DISPLAY=":0"
 
 # if [[ "$exec_type" == "extract" || "$exec_type" == "transform" ]]; then
 #     source $local/env/display.sh
